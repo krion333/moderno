ee3bfae8654dbdcfca4f66b5072eda479176538e
krion333 <avesta2012@rambler.ru>
2020-04-08 21:49:50 +0400

    added section slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function(){
 
+   $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+   });
+
+   $('.product-slider__inner').slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      arrows: false,
+    });
+
    var mixer = mixitup('.products__inner-box');
+
 
 });
